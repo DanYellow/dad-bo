99f0f9270d204db6d6d7ed15e9ab06445e41a29b
Dan42K <jeanlouis.danielo@sfr.fr>
2016-12-27 18:30:47 +0100

    add tweet controller api

---
+++ src/Admin/APIBundle/Controller/TwitterController.php
@@ -0,0 +1,42 @@
+<?php
+  namespace Admin\APIBundle\Controller;
+
+  use Abraham\TwitterOAuth\TwitterOAuth;
+
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Symfony\Component\HttpFoundation\Request;
+
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+  use Symfony\Component\HttpFoundation\JsonResponse;
+
+  /**
+  * 
+  */
+  class TwitterController extends Controller
+  {
+    
+    /**
+     * 
+     * @Route("/random_tweet")
+     * @Method({"GET"})
+     * 
+     */
+    public function getCategories(Request $request)
+    {
+      $CONSUMER_KEY = $this->getParameter('twitter_consumer_key');
+      $CONSUMER_SECRET = $this->getParameter('twitter_consumer_secret');
+
+      $ACCESS_TOKEN = $this->getParameter('twitter_access_token');
+      $ACCESS_TOKEN_SECRET = $this->getParameter('twitter_access_token_secret');
+
+      $params = ['screen_name' => 'digitwitas', 'count' => 5];
+
+      $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+      // $content = $connection->get("search/tweets", ["q" => "twitterapi"]);
+      $content = $connection->get("statuses/user_timeline", $params);
+
+      return new JSONResponse($content);
+    }
+  }
